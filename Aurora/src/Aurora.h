4aa426148d4a3e0315595c3fdb1a0e6d768406da
e01627753 <b-weber@gmx.at>
2023-11-15 22:11:58 +0100

    setup basic application and entry point

---
+++ Aurora/src/Aurora.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Aurora applications
+
+#include "Aurora/Application.h"
+
+// ------------- Entry Point -------------
+#include "Aurora/EntryPoint.h"
+// ---------------------------------------
---
+++ Aurora/src/Aurora/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef AR_PLATFORM_WINDOWS
+	#ifdef AR_BUILD_DLL
+		#define AURORA_API __declspec(dllexport)
+	#else 
+		#define AURORA_API __declspec(dllimport)
+	#endif
+#else
+	#error Aurora only supports Windows!
+#endif
