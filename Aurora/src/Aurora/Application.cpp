7da480ebe223c334954444547028dd80292a7e25
e01627753 <b-weber@gmx.at>
2023-11-23 16:09:50 +0100

    implemented window events

--- Aurora/src/Aurora/Application.cpp
+++ Aurora/src/Aurora/Application.cpp
@@ -7,14 +7,24 @@
 
 namespace Aurora {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		AR_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -28,5 +38,11 @@ namespace Aurora {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- Aurora/src/Aurora/Application.h
+++ Aurora/src/Aurora/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Aurora/Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace Aurora {
@@ -14,7 +16,11 @@ namespace Aurora {
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
